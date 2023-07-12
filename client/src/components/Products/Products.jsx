5a432e704ad378c64bb326c4a092ce217abbe004
139484952 <98007308+thiendan@users.noreply.github.com>
2023-07-12 11:08:49 -0400

    recommended product secontion

--- client/src/components/Products/Products.jsx
+++ client/src/components/Products/Products.jsx
@@ -1,5 +1,22 @@
+import "./Products.scss";
+import Product from "./Product/Product";
+
 const Products = () => {
-    return (<div>Products</div>);
+    return (
+        <div className="products-container">
+            <div className="sec-heading">Recommended Slippers</div>
+            <div className="products">
+                <Product />
+                <Product />
+                <Product />
+                <Product />
+                <Product />
+                <Product />
+                <Product />
+                <Product />
+            </div>
+        </div>
+    );
 };
 
 export default Products;
