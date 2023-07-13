c3204f15117f11bb6234efa8941b47e336c3c7bf
139484952 <98007308+thiendan@users.noreply.github.com>
2023-07-13 11:10:28 -0400

    testing api

--- client/src/components/Home/Home.jsx
+++ client/src/components/Home/Home.jsx
@@ -1,10 +1,22 @@
+import { useEffect } from "react";
+
 import "./Home.scss";
 
 import Banner from "./Banner/Banner";
 import Category from "./Category/Category";
 import Products from "../Products/Products";
+import { fetchDataFromApi } from "../../utils/api";
 
 const Home = () => {
+
+    useEffect(() => {
+        fetchDataFromApi();
+    }, []);
+
+    const getCategories = () => {
+        fetchDataFromApi("/api/categories?populate=*").then((res) => console.log(res));
+    };
+
     return (
         <div>
             <Banner />
